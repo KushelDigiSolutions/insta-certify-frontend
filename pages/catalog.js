dee35989306e5f9d4275cbc3fa173e1efcdd215c
asitmandal <asitmandal492@gmail.com>
2024-12-03 17:47:49 +0530

    this

--- pages/catalog.js
+++ pages/catalog.js
@@ -246,8 +246,10 @@ export default function catalog(pageProp) {
       }),
     })
       .then(response => response.json())
-      .then(data => console.log('Success:', data))
+      .then(data => alert(data?.message))
       .catch(error => console.error('Error:', error));
+
+      // alert(resp)
 
 
   }
