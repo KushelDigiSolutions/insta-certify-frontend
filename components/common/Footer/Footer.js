1594d1e4d7f8bb6f072c65b1f35ace2911784b5e
asitmandal <asitmandal492@gmail.com>
2024-12-26 12:09:22 +0530

    this

--- components/common/Footer/Footer.js
+++ components/common/Footer/Footer.js
@@ -173,6 +173,7 @@ export default function Footer(footerProps) {
                       <ul className="list-2 font-opensans lopl">
                         <marquee
                           
+                          
                           behavior="static"
                           direction="up"
                           scrollamount={2}
