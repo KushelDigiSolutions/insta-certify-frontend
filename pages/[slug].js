962d3ac1c1b1a648ff294c9513f4eb8fa0427c34
asitmandal <asitmandal492@gmail.com>
2025-01-10 16:46:20 +0530

    this

--- pages/[slug].js
+++ pages/[slug].js
@@ -5,7 +5,6 @@ import HeadSEO from '../components/common/Head/head';
 export default function ContentDetails(pageProp) {
 
     const pageBuilder = pageProp?.page_content?.page;
-    console.log(pageBuilder?.seo_title);
     
     return (
         <div>
@@ -17,15 +16,11 @@ export default function ContentDetails(pageProp) {
 
 export async function getServerSideProps(context) {
     const urlSlug = context.params.slug;
-    console.log(urlSlug)
     if(urlSlug != ""){
   
       try {
-        // const contentFetch = await fetch(process.env.server.api+"pages?slug=/"+urlSlug);
-        const contentFetch = await fetch(`https://admin.instacertify.com/api/pages?slug=/about`);
+        const contentFetch = await fetch(process.env.server.api+"pages?slug=/"+urlSlug);
         const contentDetial = await contentFetch.json();
-
-        console.log(contentDetial);
 
 
         if(contentDetial.page == null){
