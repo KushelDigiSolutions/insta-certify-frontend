6a59815139d267a9c4787ac88989a0a162ea4932
asitmandal <asitmandal492@gmail.com>
2025-01-13 13:04:34 +0530

    this

--- pages/404.js
+++ pages/404.js
@@ -6,15 +6,29 @@ import GlobalHeaderFooter from "../utils/common/global-header-footer";
 
 export default function NotFound() {
   return (
-    <div className="container">
+    <div className="">
       <Head>
-          <title>We've lost this page</title>
-          <meta name="description" content="Sorry, the page you are looking for doesn't exist or has been moved."></meta>
+        <title>We've lost this page</title>
+        <meta name="description" content="Sorry, the page you are looking for doesn't exist or has been moved."></meta>
       </Head>
+      <div className="aboutwrap">
+
+        <div className="aboutcont error_cont">
+
+          {/* <img src={"https://res.cloudinary.com/dgif730br/image/upload/v1729498113/WhatsApp_Image_2024-10-21_at_1.35.14_PM.jpg"} className="aboutbanner" alt="" /> */}
+
+          <div className="aboutcontent3s">
+            {/* <span>404 error</span> */}
+          </div>
+
+
+        </div>
+
+      </div>
       <div className={style.page_404}>
         <div className={style.icon_404}>
-            <span className={style.round}></span>
-            <span className={style.blur}></span>
+          <span className={style.round}></span>
+          <span className={style.blur}></span>
         </div>
         <div className={style.content}>
           <span className={style.error_show}>404 error</span>
@@ -22,20 +36,20 @@ export default function NotFound() {
           <p>Sorry, the page you are looking for doesn't exist or has been moved.</p>
           <Link className={style.buttonGoHome} href={'/'}>
             Go to Home
-          </Link> 
+          </Link>
         </div>
-      </div>    
+      </div>
     </div>
   );
 }
 
 export async function getStaticProps(context) {
   const globalSettings = await GlobalHeaderFooter();
-    return {
-      props: {
-        page_content: null,
-        navbar: globalSettings?.header,
-        footer: globalSettings?.footer
-      }
+  return {
+    props: {
+      page_content: null,
+      navbar: globalSettings?.header,
+      footer: globalSettings?.footer
+    }
   };
 }
