32da9a9faad719ff226f3585cc537dfb91bf5d4b
asitmandal <asitmandal492@gmail.com>
2025-01-25 13:23:04 +0530

    this

--- components/common/Footer/Footer.js
+++ components/common/Footer/Footer.js
@@ -162,6 +162,8 @@ export default function Footer(footerProps) {
                     </ul>
                 
                   </div>
+                  <h4 className="footer-title jk_tit">Qr Code</h4>
+                  <img className='qr_png' width="80"  src='./images/qr.png'/>
                   <div className='payments_footering'>
                   <h4 className="footer-title jk_tit">Payments</h4>
 
@@ -170,11 +172,33 @@ export default function Footer(footerProps) {
 >
 
   <div className='svg_flex'>
-  <img src="https://res.cloudinary.com/dgif730br/image/upload/v1737633038/Group_1171276061_cho16w.svg" alt=''/>
-  <img className='qr_png' width="80"  src='./images/qr.png'/>
+  <img className='razor_pay' width="170" src="https://res.cloudinary.com/dgif730br/image/upload/v1737633038/Group_1171276061_cho16w.svg" alt=''/>
+ 
   </div>
 </ul>
-</div>
+                  </div>
+                  <div className='svg_flex111'>
+                  <h4 className="footer-title jk_tit">Social Links</h4>
+                  <div className='svg_flex svg_flex11'>
+    <a href='https://www.facebook.com/share/15NtSiGZq8/?mibextid=wwXIfr' target='_blank' ><svg width="10" height="20" viewBox="0 0 10 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M2.78163 20H6.09471V10.875H8.86503L9.28341 7.325H6.09471V5.05C6.09471 4.025 6.35478 3.325 7.68906 3.325H9.39648V0.1375C9.10249 0.0999999 8.09613 0 6.92015 0C4.45513 0 2.78163 1.6625 2.78163 4.7V7.325H0V10.875H2.78163V20Z" fill="white" />
+    </svg></a>
+
+    <a href='https://x.com/instacertify?s=11' target='_blank'> <svg width="21" height="17" viewBox="0 0 21 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M20.3965 2.01939C19.661 2.35134 18.8606 2.59025 18.0362 2.6833C18.8921 2.15126 19.5331 1.31072 19.8389 0.319389C19.0357 0.819303 18.1558 1.16985 17.2382 1.35548C16.8547 0.926527 16.3909 0.5848 15.8756 0.351597C15.3604 0.118394 14.8048 -0.00128703 14.2434 1.04374e-05C11.972 1.04374e-05 10.1453 1.92634 10.1453 4.29025C10.1453 4.6222 10.1838 4.95415 10.2463 5.27353C6.84523 5.08744 3.81194 3.38744 1.79535 0.784626C1.42791 1.44128 1.23536 2.18895 1.23773 2.94986C1.23773 4.43862 1.9612 5.75134 3.06443 6.52339C2.41428 6.4966 1.77935 6.30962 1.21129 5.97768V6.03049C1.21129 8.11525 2.61978 9.84292 4.49695 10.2403C4.14449 10.336 3.78191 10.385 3.41776 10.3861C3.15096 10.3861 2.89859 10.3584 2.64381 10.3207C3.16298 12.0207 4.67482 13.2555 6.47508 13.2957C5.0666 14.45 3.30239 15.129 1.38675 15.129C1.04304 15.129 0.725772 15.1164 0.396484 15.0762C2.21357 16.2959 4.36956 17 6.6914 17C14.229 17 18.3535 10.4666 18.3535 4.79572C18.3535 4.60963 18.3535 4.42353 18.3414 4.23744C19.1394 3.62634 19.8389 2.86939 20.3965 2.01939Z" fill="white" />
+    </svg></a>
+
+    <a href='https://www.linkedin.com/company/instacertify' target='_blank'><svg width="21" height="20" viewBox="0 0 21 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M16.248 19.9687C16.248 18.9284 16.2559 17.9116 16.248 16.8948C16.2324 15.307 16.2559 13.7113 16.1699 12.1235C16.0605 10.0039 14.498 9.65189 13.0605 10.129C12.3574 10.3637 11.9902 10.9346 11.8105 11.623C11.7168 11.9984 11.6465 12.3895 11.6465 12.7806C11.6309 15.0254 11.6309 17.2624 11.6309 19.5072C11.6309 19.6636 11.6309 19.8122 11.6309 19.9921C10.2402 19.9921 8.88086 19.9921 7.49805 19.9921C7.49805 15.5494 7.49805 11.1302 7.49805 6.67184C8.80273 6.67184 10.0996 6.67184 11.4434 6.67184C11.4434 7.25064 11.4434 7.8138 11.4434 8.40825C11.7949 8.03281 12.084 7.65737 12.4355 7.36014C13.5293 6.43719 14.8262 6.18689 16.2012 6.37461C16.873 6.46847 17.5527 6.64837 18.1621 6.93777C19.2637 7.454 19.8184 8.45518 20.0684 9.60496C20.2402 10.4028 20.3574 11.224 20.373 12.0375C20.4121 14.603 20.3887 17.1763 20.3887 19.7418C20.3887 19.8044 20.3809 19.8748 20.373 19.9687C19.0137 19.9687 17.6543 19.9687 16.248 19.9687Z" fill="white" />
+      <path d="M0.740234 6.66406C2.11523 6.66406 3.45117 6.66406 4.82617 6.66406C4.82617 11.1068 4.82617 15.5417 4.82617 20C3.4668 20 2.12305 20 0.740234 20C0.740234 15.5651 0.740234 11.1302 0.740234 6.66406Z" fill="white" />
+      <path d="M5.17778 2.40907C5.16996 3.73876 4.09184 4.84161 2.79496 4.84943C1.48246 4.84161 0.388713 3.71529 0.396526 2.36996C0.404338 1.06375 1.48246 0 2.78715 0C4.1309 0 5.18559 1.06375 5.17778 2.40907Z" fill="white" />
+    </svg></a>
+
+   <a href='https://pin.it/37GLjxHab'><img className='pinterest' width="21" height="20" src='./images/pint.svg'/></a>
+
+
+                </div>  
+                </div>
                 </div>
 
 
@@ -401,7 +425,7 @@ export default function Footer(footerProps) {
           <div  className="footer-bottom footer-line">
             <div className='foot_map'>
               <ul className='foot_site'>
-                <div className='svg_flex'>
+                {/* <div className='svg_flex'>
     <a href='https://www.facebook.com/share/15NtSiGZq8/?mibextid=wwXIfr' target='_blank' ><svg width="10" height="20" viewBox="0 0 10 20" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path d="M2.78163 20H6.09471V10.875H8.86503L9.28341 7.325H6.09471V5.05C6.09471 4.025 6.35478 3.325 7.68906 3.325H9.39648V0.1375C9.10249 0.0999999 8.09613 0 6.92015 0C4.45513 0 2.78163 1.6625 2.78163 4.7V7.325H0V10.875H2.78163V20Z" fill="white" />
     </svg></a>
@@ -419,7 +443,7 @@ export default function Footer(footerProps) {
    <a href='https://pin.it/37GLjxHab'><img className='pinterest' width="21" height="20" src='./images/pint.svg'/></a>
 
 
-  </div>
+                </div>    */}
                 <li><a href='https://instacertify.com/privacy-policy' >PRIVACY POLICY</a></li>
                 <li><a href="https://instacertify.com/refund-policy" >REFUND POLICY</a></li>
                 <li><a href="https://instacertify.com/terms-and-condition">TERMS AND CONDITIONS</a></li>
